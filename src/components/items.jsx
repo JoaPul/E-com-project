1004deddc8aad91a970e33f44fa963149ed9c42e
Joaquin Arroyo <joaquinarroyo@outlook.com>
2022-06-08 17:58:41 -0500

    Changes in loading screen

--- src/components/items.jsx
+++ src/components/items.jsx
@@ -6,27 +6,23 @@ import '../styles/items.css'
 
 const Items = ({ esc }) => {
   const { itemss, loading, error } = useGetData()
-  const [arr, setArr] = useState(itemss)
-
-  // const [neww, setNew] = useState([])
+  const [arr, setArr] = useState([])
 
   if (error) {
     return (<p>{error}</p>)
   }
   // TODO añadir spiner
   if (loading) {
-    return (<p>...Loading</p>)
+    return (
+      <>
+        <p className='spinner-1'>...Loading page</p>
+      </>
+    )
   }
 
-  // const rend =
-
-  // useEffect(() => {
-  //   if (loading) {
-  //     neww
-  //   } else {
-  //     setArr(itemss)
-  //   }
-  // }, [loading])
+  setTimeout(() => {
+    setArr(itemss)
+  }, 4000)
 
   const searchItems = (val) => {
     setArr(itemss.filter(item => {
@@ -38,18 +34,19 @@ const Items = ({ esc }) => {
     <div className='cont-items'>
       {esc ? <input type='text' placeholder='Busca un producto...' onChange={(event) => searchItems(event.target.value)} /> : ''}
       <section className='div-items'>
-        {arr.map((index, key) => (
-          <div key={key} className='conte-Items'>
-            <article className='card'>
-              <img src={index.image ? index.image : index.images ? index.images : 'https://pbs.twimg.com/media/FFTMAFmWYAIwYaY?format=jpg&name=large'} alt='imagen de articulo' />
-              <p>{index.product_name}</p>
-            </article>
-          </div>
-        ))}
+        {arr !== itemss
+          ? <p className='spinner-1'>...Loading page</p>
+          : arr.map((index, key) => (
+            <div key={key} className='conte-Items'>
+              <article className='card'>
+                <img src={index.image ? index.image : index.images ? index.images : 'https://pbs.twimg.com/media/FFTMAFmWYAIwYaY?format=jpg&name=large'} alt='imagen de articulo' />
+                <p>{index.product_name}</p>
+              </article>
+            </div>
+          ))}
       </section>
     </div>
   )
 }
 
 export default Items
-// export { Buscador }
