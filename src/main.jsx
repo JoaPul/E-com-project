5e723fd6fb0b63fdc336397400da78004fc11392
Joaquin Arroyo <joaquinarroyo@outlook.com>
2022-06-08 17:59:30 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 // Styles
 import './styles/index.css'
 // components
