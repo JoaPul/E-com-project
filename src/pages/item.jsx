e95a3c431bc88d11392e8a79f3e412577b38d875
Joaquin Arroyo <87846400+JoaPul@users.noreply.github.com>
2022-07-02 22:43:23 -0500

    starting the responsive adjustings(item page)

--- src/pages/item.jsx
+++ src/pages/item.jsx
@@ -8,32 +8,41 @@ const Item = () => {
   const { items, addToCart, addToWish } = useAppContext()
 
   return (
-    <section className='articuloInd'>
-      <div className='imgArt'>
-        <img src={items[1]} alt='Imagen de articulo' />
+    <section className='cont-Item'>
+      <div className='prodDet'>
+        <h2>Product Details</h2>
       </div>
-      <div className='infArt'>
-        <h1>{items[0].product_name} | <span className='Dispon' style={items[0].isActive ? { color: '#198755' } : { color: 'Red' }}>{items[0].isActive ? 'Available' : 'Not available at the moment'}</span></h1>
-        <hr />
-        <p style={{ fontSize: '28px' }}>${items[0].price} </p>
-        <hr />
-        <p><span style={{ fontWeight: '1000' }}>Category:</span> {items[0].category}</p>
-        <hr />
-        <p><span style={{ fontWeight: '1000' }}>Brand:</span> {items[0].brand}</p>
-        <hr />
-        <p style={{ width: '70%' }}><span style={{ fontWeight: '1000' }}>Description:</span> {items[0].description}</p>
-        <br />
+      <section className='articuloInd'>
+        <div className='img-des'>
+          <div className='imgArt'>
+            <img src={items[1]} alt='Imagen de articulo' />
+          </div>
+          <div className='infArt'>
+            <h1>{items[0].product_name} | <span className='Dispon' style={items[0].isActive ? { color: '#198755' } : { color: 'Red' }}>{items[0].isActive ? 'Available' : 'Not available at the moment'}</span></h1>
+            <hr />
+            <p style={{ fontSize: '20px' }}>${items[0].price} </p>
+            <hr />
+            <p><span style={{ fontWeight: '1000' }}>Category:</span> {items[0].category}</p>
+            <hr />
+            <p><span style={{ fontWeight: '1000' }}>Brand:</span> {items[0].brand}</p>
+            <hr />
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <p style={{ width: '80%' }}><span style={{ fontWeight: '1000' }}>Description:</span> {items[0].description}</p>
+            </div>
+            <br />
+          </div>
+        </div>
         <div className='actions'>
           <button onClick={() => addToCart(items)}>
-            <img src='../../src/assets/CarritoBlanc.png' alt='Imagen carrito de compras' />
+            <img src='https://i.postimg.cc/qhB9XLLD/Carrito-Blanc.png' alt='Imagen carrito de compras' />
             Add to cart
           </button>
           <button onClick={() => addToWish(items)}>
-            <img src='../../src/assets/CoraW.png' alt='Icono de vfavoritos' />
+            <img src='https://i.postimg.cc/YLhZTCT1/CoraW.png' alt='Icono de vfavoritos' />
             Add to Wish List
           </button>
         </div>
-      </div>
+      </section>
     </section>
   )
 }
